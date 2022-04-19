0d469c173361a9e6a26ed4cd1c120e8f4f6b2d0d
Miquel Espinosa <miquelespinosa2@gmail.com>
2022-04-19 13:28:07 +0200

    created the navbar skeleton

--- src/App.js
+++ src/App.js
@@ -17,10 +17,7 @@ function App() {
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/forgot-password' element={<ForgotPassword />} />
-
-
       </Routes>
-
     </Router>
 
   </>
