2099832545d810560c1e86ff48654b8d76301f2d
Miquel Espinosa <miquelespinosa2@gmail.com>
2022-04-20 12:17:36 +0200

    added the signin auth

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {useState} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
+import {getAuth, signInWithEmailAndPassword} from 'firebase/auth'
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -22,6 +23,24 @@ function SignIn() {
 
   }
 
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const auth = getAuth()
+
+      const userCredentials = await signInWithEmailAndPassword(auth, email, password)
+
+      if(userCredentials.user) {
+        navigate('/')
+      }
+
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
+
   return (
    <>
     <div className="pageContainer">
@@ -31,7 +50,7 @@ function SignIn() {
         </p>
       </header>
 
-      <form>
+      <form onSubmit={onSubmit}>
         <input type="email" className="emailInput"
         placeholder="Email" id="email" value={email}
         onChange={onChange}/>
