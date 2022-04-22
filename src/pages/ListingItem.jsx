aba3fd5449fb6c4c1b1067af7452c9a00b5ffb62
Miquel Espinosa <miquelespinosa2@gmail.com>
2022-04-22 14:42:44 +0200

    finished the listing item component

--- src/pages/ListingItem.jsx
+++ src/pages/ListingItem.jsx
@@ -4,7 +4,7 @@ import {ReactComponent as DeleteIcon} from '../assets/svg/deleteIcon.svg'
 import bedIcon from '../assets/svg/bedIcon.svg'
 import bathtubIcon from '../assets/svg/bathtubIcon.svg'
 
-function ListingItem({ listing, id }) {
+function ListingItem({ listing, id, onDelete }) {
   return (
     <li className='categoryListing'>
       <Link to={`/category/${listing.type}/${id}`} className='categoryListingLink'>
@@ -41,6 +41,12 @@ function ListingItem({ listing, id }) {
           </div>
         </div>
       </Link>
+
+
+      {onDelete && (
+        <DeleteIcon className='removeIcon' fill='rgb(231, 76, 60)'
+        onClick={() => onDelete(listing.id, listing.name)}/>
+      )}
     </li>
   )
 }
