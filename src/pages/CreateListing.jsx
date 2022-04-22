46b737b157d4c2b30f70db7c3ab703db703f8a30
Miquel Espinosa <miquelespinosa2@gmail.com>
2022-04-22 17:51:10 +0200

    preparing the mounting for memory leaks

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -3,6 +3,8 @@ import {useState, useEffect} from 'react'
 import {getAuth, onAuthStateChanged} from 'firebase/auth'
 import {useNavigate} from 'react-router-dom'
 import Spinner from '../components/Spinner'
+import {useRef} from 'react'
+
 
 function CreateListing() {
   const [geolocationEnabled, setGeolocationEnabled] = useState(true)
@@ -21,6 +23,25 @@ function CreateListing() {
     longitude: 0,
   })
 
+
+  const auth = getAuth()
+  const navigate = useNavigate()
+  const isMounted = useRef(true)
+
+
+  useEffect(() => {
+    if(isMounted) {
+      onAuthStateChanged(auth, (user) => {
+        if(user) {
+          setFormData({...formData,userRef: user.uid})
+        }
+      })
+    }
+
+    return () => {
+      isMounted.current = false
+    }
+  }, [isMounted])
   return (
     <div>Create</div>
   )
