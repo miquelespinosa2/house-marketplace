8b4b2c8ef6a7aa17343fdd976e07b2cc7324bc7a
Miquel Espinosa <miquelespinosa2@gmail.com>
2022-04-22 13:22:47 +0200

    fetching from firebase and creating queries

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,6 +1,35 @@
 import React from 'react'
+import {useEffect, useState} from 'react'
+import {useParams} from 'react-router-dom'
+import {collection, getDocs, query, where, orderBy, limit, startAfter} from 'firebase/firestore'
+import {db} from '../firebase.config'
+import {toast} from 'react-toastify'
+import Spinner from '../components/Spinner'
+
 
 function Category() {
+  const [listings, setListings] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  const params = useParams()
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        // Get reference
+        const listingsRef = collection(db, 'listings')
+
+        // Create query
+        const q = query(listingsRef, where('type', '==', params.categoryName),
+        orderBy('timestamp', 'desc'),
+        limit(10))
+      } catch (error) {
+
+      }
+    }
+
+    fetchListings()
+  })
   return (
     <div>Category</div>
   )
