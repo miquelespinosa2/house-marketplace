6cafeb35d113353d9533ee72e5f261147d0afa79
Miquel Espinosa <miquelespinosa2@gmail.com>
2022-04-22 17:47:46 +0200

    imports for the listing and useState

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import {useState, useEffect} from 'react'
+import {getAuth, onAuthStateChanged} from 'firebase/auth'
+import {useNavigate} from 'react-router-dom'
+import Spinner from '../components/Spinner'
 
 function CreateListing() {
+  const [geolocationEnabled, setGeolocationEnabled] = useState(true)
+  const [formData, setFormData] = useState({
+    type: 'rent',
+    name: '',
+    bedrooms: 1,
+    bathrooms: 1,
+    parking: false,
+    furnished: false,
+    address: '',
+    offer: false,
+    discountedPrice: 0,
+    images: {},
+    latitude: 0,
+    longitude: 0,
+  })
+
   return (
-    <div>CreateListing</div>
+    <div>Create</div>
   )
 }
 
