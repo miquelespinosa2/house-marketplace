299df04a78c5c626eb2423c70104f5bdcd914448
Miquel Espinosa <miquelespinosa2@gmail.com>
2022-04-26 18:18:50 +0200

    checking whether a landlord exists

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -13,11 +13,37 @@ function Contact() {
   const params = useParams()
 
   useEffect(() => {
+    const getLandlord = async () => {
+      const docRef = doc(db, 'users', params.landlordId)
+      const docSnap = await getDoc(docRef)
 
-  }, [params.landlord])
+      if(docSnap.exists()) {
+        setLandlord(docSnap.data())
+      } else {
+        toast.error('Could not get landlord data')
+      }
+    }
+
+    getLandlord()
+
+  }, [params.landlordId])
 
   return (
-    <div>Contact</div>
+    <div className='pageContainer'>
+      <header>
+        <p className="pageHeader">
+          Contact Landlord
+        </p>
+      </header>
+
+      {landlord !== null && (
+        <main>
+          <div className="contactLandlord">
+            <p className="landlordName">Contact {landlord?.name}</p>
+          </div>
+        </main>
+      )}
+    </div>
   )
 }
 
