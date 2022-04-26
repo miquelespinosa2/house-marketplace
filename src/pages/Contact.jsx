cec2f7e2343ed2763625e8b7b6dad540065e6b72
Miquel Espinosa <miquelespinosa2@gmail.com>
2022-04-26 18:13:44 +0200

    imports

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import {useState, useEffect} from 'react'
+import {useParams, useSearchParams} from 'react-router-dom'
+import {doc, getDoc} from 'firebase/firestore'
+import {db} from '../firebase.config'
+import {toast} from 'react-toastify'
 
 function Contact() {
   return (
