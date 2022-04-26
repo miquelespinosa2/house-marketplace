d35bd91dcd9fdd3ad3330c8c50d7ef3905252d36
Miquel Espinosa <miquelespinosa2@gmail.com>
2022-04-26 18:06:02 +0200

    added frontend for bedrooms, sale, discount, address

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -52,16 +52,31 @@ return <main>
   {shareLinkCopied && <p className='linkCopied'>Link Copied!</p>}
 
   <div className="listingDetails">
-  <p className='listingName'>
-          {listing.name} - $
-          {listing.offer
-            ? listing.discountedPrice
-                .toString()
-                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-            : listing.regularPrice
-                .toString()
-                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
-        </p>
+    <p className='listingName'>
+      {listing.name} - $
+      {listing.offer
+        ? listing.discountedPrice
+            .toString()
+            .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+        : listing.regularPrice
+            .toString()
+            .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+    </p>
+    <p className="listingLocation">{listing.location}</p>
+    <p className="listingType">
+      For {listing.type === 'rent' ? 'Rent' : 'Sale'}
+    </p>
+    {listing.offer && (
+      <p className='discountPrice'>
+        ${listing.regularPrice - listing.discountedPrice} discount
+      </p>
+    )}
+
+    <ul className="listingDetailsList">
+      <li>
+        {listing.bedrooms > 1 ? `${listing.bedrooms} Bedrooms` : '1 Bedroom'}
+      </li>
+    </ul>
   </div>
 </main>
 
