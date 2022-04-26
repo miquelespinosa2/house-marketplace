4a402255f2d7470d62dfb9ec1d01f4f2bb1c22af
Miquel Espinosa <miquelespinosa2@gmail.com>
2022-04-26 17:50:14 +0200

    added imports

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,4 +1,12 @@
 import React from 'react'
+import {useState, useEffect} from 'react'
+import {Link, useNavigate, useParams} from 'react-router-dom'
+import {getDoc, doc} from 'firebase/firestore'
+import {getAuth} from 'firebase/auth'
+import {db} from '../firebase.config'
+import Spinner from '../components/Spinner'
+import shareIcon from '../assets/svg/shareIcon.svg'
+
 
 function Listing() {
   return (
