b66f09dce9bd3f0765980c60e2341a484642ee02
Miquel Espinosa <miquelespinosa2@gmail.com>
2022-04-26 18:56:20 +0200

    imported leaflet

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import {useState, useEffect} from 'react'
-import {useParams, useSearchParams} from 'react-router-dom'
-import {doc, getDoc} from 'firebase/firestore'
-import {db} from '../firebase.config'
-import {toast} from 'react-toastify'
+
+import { useState, useEffect } from 'react'
+import { useParams, useSearchParams } from 'react-router-dom'
+import { doc, getDoc } from 'firebase/firestore'
+import { db } from '../firebase.config'
+import { toast } from 'react-toastify'
 
 function Contact() {
   const [message, setMessage] = useState('')
   const [landlord, setLandlord] = useState(null)
+  // eslint-disable-next-line
   const [searchParams, setSearchParams] = useSearchParams()
 
   const params = useParams()
@@ -17,7 +18,7 @@ function Contact() {
       const docRef = doc(db, 'users', params.landlordId)
       const docSnap = await getDoc(docRef)
 
-      if(docSnap.exists()) {
+      if (docSnap.exists()) {
         setLandlord(docSnap.data())
       } else {
         toast.error('Could not get landlord data')
@@ -25,7 +26,6 @@ function Contact() {
     }
 
     getLandlord()
-
   }, [params.landlordId])
 
   const onChange = (e) => setMessage(e.target.value)
@@ -33,28 +33,39 @@ function Contact() {
   return (
     <div className='pageContainer'>
       <header>
-        <p className="pageHeader">
-          Contact Landlord
-        </p>
+        <p className='pageHeader'>Contact Landlord</p>
       </header>
 
       {landlord !== null && (
         <main>
-          <div className="contactLandlord">
-            <p className="landlordName">Contact {landlord?.name}</p>
+          <div className='contactLandlord'>
+            <p className='landlordName'>Contact {landlord?.name}</p>
           </div>
 
-        <form  className="messageform">
-          <div className="messageDiv">
-            <label htmlFor="message" className="messageLabel">
-              Message
-            </label>
-            <textarea name="message" id="messge" className='textarea'
-            value={message} onChange={onChange}>
+          <form className='messageForm'>
+            <div className='messageDiv'>
+              <label htmlFor='message' className='messageLabel'>
+                Message
+              </label>
+              <textarea
+                name='message'
+                id='message'
+                className='textarea'
+                value={message}
+                onChange={onChange}
+              ></textarea>
+            </div>
 
-            </textarea>
-          </div>
-        </form>
+            <a
+              href={`mailto:${landlord.email}?Subject=${searchParams.get(
+                'listingName'
+              )}&body=${message}`}
+            >
+              <button type='button' className='primaryButton'>
+                Send Message
+              </button>
+            </a>
+          </form>
         </main>
       )}
     </div>
