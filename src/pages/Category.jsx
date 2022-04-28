e40e2ee8b9210e80f83617b978372fb06ec0eb99
Miquel Espinosa <miquelespinosa2@gmail.com>
2022-04-28 12:30:50 +0200

    loading more pagination

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -32,7 +32,7 @@ function Category() {
 
         const lastVisible = querySnap.docs[querySnap.docs.length - 1]
         setLastFetchedListing(lastVisible)
-        
+
         const listings = []
 
         querySnap.forEach((doc) => {
@@ -51,6 +51,42 @@ function Category() {
 
     fetchListings()
   }, [params.categoryName])
+
+
+  // Pagination / Load More
+  const onFetchMoreListings = async () => {
+    try {
+      // Get reference
+      const listingsRef = collection(db, 'listings')
+
+      // Create query
+      const q = query(listingsRef, where('type', '==', params.categoryName),
+      orderBy('timestamp', 'desc'),
+      startAfter(lastFetchedListing),
+      limit(10)
+      )
+
+      // Execute query
+      const querySnap = await getDocs(q)
+
+      const lastVisible = querySnap.docs[querySnap.docs.length - 1]
+      setLastFetchedListing(lastVisible)
+
+      const listings = []
+
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data()
+        })
+      })
+
+      setListings((prevState) => [...prevState, ...listings])
+      setLoading(false)
+    } catch (error) {
+      toast.error('Could not fetch listings')
+    }
+  }
   return (
     <div className='category'>
       <header>
